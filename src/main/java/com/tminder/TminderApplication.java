249ff4ccd8c9edd9dbaa76b79fa1f21533b6b019
Nikhil Bhawsar <nikhilandmanit@gmail.com>
2026-02-21 21:18:17 +0530

    chore: initial project setup with gradle and clean architecture structure

---
+++ src/main/java/com/tminder/TminderApplication.java
@@ -0,0 +1,13 @@
+package com.tminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TminderApplication.class, args);
+    }
+
+}
